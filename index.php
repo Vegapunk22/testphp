cf965b0303f8c20f4f58eb08065716371518d6c8
Vegapunk <37366463+Vegapunk22@users.noreply.github.com>
2019-02-18 01:32:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,16 +2,14 @@
 $servername = "mysql";
 $username = "root";
 $password = "root";
+$dbname = "middleearth";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
-echo "Connected successfully";
-
+}
 $sql = "SELECT id, nom, race, description FROM personnage";
 $result = $conn->query($sql);
 

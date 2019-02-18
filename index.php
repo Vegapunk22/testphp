da93046498cb759c690f72314b31deb4a950c480
Vegapunk <37366463+Vegapunk22@users.noreply.github.com>
2019-02-18 01:25:15 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-phpinfo();
-/* $servername = "localhost";
-$username = "username";
-$password = "password";
+$servername = "mysql";
+$username = "root";
+$password = "root";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -11,5 +10,19 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully"; */
+echo "Connected successfully";
+
+$sql = "SELECT id, nom, race, description FROM personnage";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Nom: " . $row["nom"]. " - Race: " . $row["race"]. " - Description: " . $row["description"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
 ?>
